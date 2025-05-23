cc32d0d8a8ab08585c803e9091efa7036d2cef5a
Lancelot de Ferrière <wraitii@gmail.com>
2025-05-23 15:05:05 +0200

    fix duplicate barretenbergs

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+    resolve: {
+        dedupe: ["@aztec/bb.js"],
+    },
     plugins: [
         vue({
             template: {
